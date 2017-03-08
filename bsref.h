186512125b4d0589e8ab77e7e1f5b0ec0d312bd1
Pavel Chistyakov <p000000000001@gmail.com>
2017-03-08 08:29:06 +0200

    Remove copyright

--- bsref.h
+++ bsref.h
@@ -1,17 +1,3 @@
-//Copyright 2017 by Pavel Chistyakov
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-
 #include "aref.h"
 #include "bsfield.h"
 
@@ -23,7 +9,7 @@ struct bsref
 	const bsfield*	fields;
 	void*			object;
 	//
-	inline operator bool() { return fields != 0; }
+	inline operator bool() const { return fields != 0; }
 	//
 	void			clear() const;
 	const bsfield*	find(const char* name) const { return fields->find(name); }
